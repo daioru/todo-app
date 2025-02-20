302d2ec8ff361659f1e73ee6c08fa76ae63fff06
daioru <daioru.xyz@gmail.com>
2025-02-20 18:23:52 +0300

    Add DB integration

--- cmd/todo-app/main.go
+++ cmd/todo-app/main.go
@@ -5,10 +5,28 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/daioru/todo-app/internal/config"
+	"github.com/daioru/todo-app/internal/pkg/db"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	if err := config.ReadConfigYML("config.yml"); err != nil {
+		log.Fatal("Failed init configuration")
+	}
+	cfg := config.GetConfigInstance()
+
+	db, err := db.ConnectDB(&cfg.DB)
+	if err != nil {
+		log.Fatalf("sqlx_Open error: %v", err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatalf("Error testing db connection: %v", err)
+	}
+
 	r := gin.Default()
 	r.SetTrustedProxies(nil)
 
