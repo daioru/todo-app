fd12fefacbb461d35fa46232697fbae8184d532a
daioru <daioru.xyz@gmail.com>
2025-02-21 14:44:13 +0300

    Models refactoring

--- internal/models/task.go
+++ internal/models/task.go
@@ -2,13 +2,6 @@ package models
 
 import "time"
 
-type User struct {
-	ID           int       `db:"id" json:"id"`
-	Username     string    `db:"username" json:"username"`
-	PasswordHash string    `db:"password_hash" json:"-"`
-	CreatedAt    time.Time `db:"created_at" json:"created_at"`
-}
-
 type Task struct {
 	ID          int       `db:"id" json:"id"`
 	UserID      int       `db:"user_id" json:"user_id"`
