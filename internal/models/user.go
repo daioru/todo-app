f3c4c7237fab9aaf7358676665e704c183108b08
daioru <daioru.xyz@gmail.com>
2025-02-21 15:30:22 +0300

    Add password field to User struct

--- internal/models/user.go
+++ internal/models/user.go
@@ -5,6 +5,7 @@ import "time"
 type User struct {
 	ID           int       `db:"id" json:"id"`
 	Username     string    `db:"username" json:"username"`
+	Password     string    `db:"-" json:"password"`
 	PasswordHash string    `db:"password_hash" json:"-"`
 	CreatedAt    time.Time `db:"created_at" json:"created_at"`
 }
