2e4cdc5baef678e17a5b2d156cbfe2e158d2b47e
daioru <daioru.xyz@gmail.com>
2025-02-22 14:58:04 +0300

    Update task model JSON parsing

--- internal/models/task.go
+++ internal/models/task.go
@@ -3,10 +3,10 @@ package models
 import "time"
 
 type Task struct {
-	ID          int       `db:"id" json:"id"`
+	ID          int       `db:"id" json:"-"`
 	UserID      int       `db:"user_id" json:"user_id"`
 	Title       string    `db:"title" json:"title"`
 	Description string    `db:"description" json:"description"`
 	Status      string    `db:"status" json:"status"`
-	CreatedAt   time.Time `db:"created_at" json:"created_at"`
+	CreatedAt   time.Time `db:"created_at" json:"-"`
 }
