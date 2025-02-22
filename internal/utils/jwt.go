c22556b581e0daaf68fc454a3d35d5d175ae9b53
daioru <daioru.xyz@gmail.com>
2025-02-22 19:50:48 +0300

    Add JWT utils

---
+++ internal/utils/jwt.go
@@ -0,0 +1,43 @@
+package utils
+
+import (
+	"errors"
+	"time"
+
+	"github.com/golang-jwt/jwt/v4"
+)
+
+func GenerateToken(userID int, secredKey string) (string, error) {
+	claims := jwt.MapClaims{
+		"user_id": userID,
+		"exp":     time.Now().Add(time.Hour * 72).Unix(),
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString(secredKey)
+}
+
+func ValidateToken(tokenString, secredKey string) (int, error) {
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, errors.New("unexpected signing method")
+		}
+		return secredKey, nil
+	})
+
+	if err != nil || !token.Valid {
+		return 0, errors.New("invalid token")
+	}
+
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if !ok {
+		return 0, errors.New("invalid token claims")
+	}
+
+	userID, ok := claims["user_id"].(int)
+	if !ok {
+		return 0, errors.New("invalid user_id in token")
+	}
+
+	return userID, nil
+}
