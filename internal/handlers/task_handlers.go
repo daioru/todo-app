59722bf0a07d70a672f13a043c9de0c3da76d277
daioru <daioru.xyz@gmail.com>
2025-02-22 15:09:09 +0300

    Add task handlers

---
+++ internal/handlers/task_handlers.go
@@ -0,0 +1,59 @@
+package handlers
+
+import (
+	"net/http"
+
+	"github.com/daioru/todo-app/internal/models"
+	"github.com/daioru/todo-app/internal/services"
+	"github.com/gin-gonic/gin"
+)
+
+type TaskHandler struct {
+	service *services.TaskService
+}
+
+func NewTaskHandler(taskService *services.TaskService) *TaskHandler {
+	return &TaskHandler{service: taskService}
+}
+
+func (h *TaskHandler) CreateTask(c *gin.Context) {
+	var task models.Task
+	if err := c.ShouldBindJSON(&task); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid request"})
+		return
+	}
+
+	if err := h.service.CreateTask(&task); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+	}
+
+	c.JSON(http.StatusCreated, task)
+}
+
+func (h *TaskHandler) GetTasks(c *gin.Context) {
+	userID := c.GetInt("user_id")
+	tasks, err := h.service.GetTasksByUser(userID)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"errors": err.Error()})
+	}
+
+	c.JSON(http.StatusOK, tasks)
+}
+
+func (h *TaskHandler) UdpateTask(c *gin.Context) {
+	var task models.Task
+	if err := c.ShouldBindJSON(&task); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid request"})
+		return
+	}
+
+	if err := h.service.UpdateTask(&task); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"errro": err.Error()})
+	}
+
+	c.JSON(http.StatusOK, task)
+}
+
+func (h *TaskHandler) DeleteTask(c *gin.Context) {
+
+}
