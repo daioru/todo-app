72e89034b7bdee22dd219adb42c04fdcc41defd5
daioru <daioru.xyz@gmail.com>
2025-02-23 12:08:57 +0300

    Add loging to task_repository

--- internal/repository/task_repository.go
+++ internal/repository/task_repository.go
@@ -8,7 +8,10 @@ import (
 
 	"github.com/Masterminds/squirrel"
 	"github.com/jmoiron/sqlx"
+	"github.com/sirupsen/logrus"
 )
+
+var log = logrus.New()
 
 type TaskRepository struct {
 	db *sqlx.DB
@@ -29,38 +32,81 @@ func (r *TaskRepository) CreateTask(task *models.Task) error {
 		Suffix("RETURNING id, created_at").
 		ToSql()
 	if err != nil {
+		log.WithFields(logrus.Fields{
+			"user_id":     task.UserID,
+			"task_id":     task.UserID,
+			"title":       task.Title,
+			"description": task.Description,
+			"status":      task.Status,
+		}).Errorf("Failed to build SQL query: %v", err)
 		return err
 	}
-	return r.db.QueryRow(query, args...).Scan(&task.ID, &task.CreatedAt)
+
+	err = r.db.QueryRow(query, args...).Scan(&task.ID, &task.CreatedAt)
+	if err != nil {
+		log.WithFields(logrus.Fields{
+			"query": query,
+			"args":  args,
+		}).Errorf("DB execution error: %v", err)
+		return err
+	}
+
+	return nil
 }
 
 func (r *TaskRepository) GetTaskByID(id int) (*models.Task, error) {
 	var task models.Task
+
 	query, args, err := r.sq.Select("id", "user_id", "title", "description", "status", "created_at").
 		From("tasks").
 		Where(squirrel.Eq{"id": id}).
 		ToSql()
 	if err != nil {
-		return nil, err
+		log.WithFields(logrus.Fields{
+			"task_id": id,
+		}).Errorf("Failed to build SQL query: %v", err)
+		return &task, err
 	}
+
 	err = r.db.Get(&task, query, args...)
-	if err == sql.ErrNoRows {
-		return nil, nil
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return nil, nil
+		}
+		log.WithFields(logrus.Fields{
+			"query": query,
+			"args":  args,
+		}).Errorf("DB execution error: %v", err)
+		return &task, err
 	}
-	return &task, err
+
+	return &task, nil
 }
 
 func (r *TaskRepository) GetTasksByUserID(userID int) ([]models.Task, error) {
 	var tasks []models.Task
+
 	query, args, err := r.sq.Select("id", "user_id", "title", "description", "status", "created_at").
 		From("tasks").
 		Where(squirrel.Eq{"user_id": userID}).
 		ToSql()
 	if err != nil {
-		return nil, err
+		log.WithFields(logrus.Fields{
+			"userID": userID,
+		}).Errorf("Failed to build SQL query: %v", err)
+		return tasks, err
 	}
+
 	err = r.db.Select(&tasks, query, args...)
-	return tasks, err
+	if err != nil {
+		log.WithFields(logrus.Fields{
+			"query": query,
+			"args":  args,
+		}).Errorf("DB execution error: %v", err)
+		return tasks, err
+	}
+
+	return tasks, nil
 }
 
 func (r *TaskRepository) DeleteTask(taskID, userID int) error {
@@ -71,10 +117,23 @@ func (r *TaskRepository) DeleteTask(taskID, userID int) error {
 		}).
 		ToSql()
 	if err != nil {
+		log.WithFields(logrus.Fields{
+			"task_id": taskID,
+			"user_id": userID,
+		}).Errorf("Failed to build SQL query: %v", err)
 		return err
 	}
+
 	_, err = r.db.Exec(query, args...)
-	return err
+	if err != nil {
+		log.WithFields(logrus.Fields{
+			"query": query,
+			"args":  args,
+		}).Errorf("DB execution error: %v", err)
+		return err
+	}
+
+	return nil
 }
 
 func (r *TaskRepository) UpdateTask(task *models.Task) error {
@@ -82,15 +141,25 @@ func (r *TaskRepository) UpdateTask(task *models.Task) error {
 		Set("title", task.Title).
 		Set("description", task.Description).
 		Set("status", task.Status).
-		Where(squirrel.And{
-			squirrel.Eq{"id": task.ID},
-			squirrel.Eq{"user_id": task.UserID},
-		}).Suffix("RETURNING created_at").
+		Where(squirrel.Eq{"id": task.ID, "user_id": task.UserID}).
+		Suffix("RETURNING created_at").
 		ToSql()
 	if err != nil {
+		log.WithFields(logrus.Fields{
+			"task_id": task.ID,
+			"user_id": task.UserID,
+		}).Errorf("Failed to build SQL query: %v", err)
 		return err
 	}
 
 	err = r.db.Get(&task.CreatedAt, query, args...)
-	return err
+	if err != nil {
+		log.WithFields(logrus.Fields{
+			"query": query,
+			"args":  args,
+		}).Errorf("DB execution error: %v", err)
+		return err
+	}
+
+	return nil
 }
