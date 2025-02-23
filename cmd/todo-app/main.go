59ff19cdd1584e84a9f56c5c6447b0bf727614ae
daioru <daioru.xyz@gmail.com>
2025-02-23 18:20:25 +0300

    Add zerolog logger to todo-app main.go

--- cmd/todo-app/main.go
+++ cmd/todo-app/main.go
@@ -2,12 +2,12 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 	"os"
 
 	"github.com/daioru/todo-app/internal/config"
 	"github.com/daioru/todo-app/internal/handlers"
+	"github.com/daioru/todo-app/internal/logger"
 	"github.com/daioru/todo-app/internal/middlewares"
 	"github.com/daioru/todo-app/internal/pkg/db"
 	"github.com/daioru/todo-app/internal/repository"
@@ -17,32 +17,35 @@ import (
 )
 
 func main() {
+	logger.InitLogger()
+	log := logger.GetLogger()
+
 	if err := config.ReadConfigYML("config.yml"); err != nil {
-		log.Fatal("Failed init configuration")
+		log.Fatal().Msg("Failed init configuration")
 	}
 	cfg := config.GetConfigInstance()
 
 	db, err := db.ConnectDB(&cfg.DB)
 	if err != nil {
-		log.Fatalf("sqlx_Open error: %v", err)
+		log.Fatal().Msgf("sqlx_Open error: %v", err)
 	}
 	defer db.Close()
 
 	err = db.Ping()
 	if err != nil {
-		log.Fatalf("Error testing db connection: %v", err)
+		log.Fatal().Msgf("Error testing db connection: %v", err)
 	}
 
 	userRepo := repository.NewUserRepository(db)
 
 	err = godotenv.Load()
 	if err != nil {
-		log.Fatal("Error loading .env file")
+		log.Fatal().Msg("Error loading .env file")
 	}
 
 	jwtSecret := os.Getenv("JWTSECRET")
 	if jwtSecret == "" {
-		log.Fatal("No jwtSecret in .env")
+		log.Fatal().Msg("No jwtSecret in .env")
 	}
 
 	authService := services.NewAuthService(userRepo, []byte(jwtSecret))
@@ -77,6 +80,6 @@ func main() {
 	port := ":8080"
 	fmt.Println("Server is running on port" + port)
 	if err := r.Run(port); err != nil {
-		log.Fatal("Failed to start server: ", err)
+		log.Fatal().Msgf("Failed to start server: %v", err)
 	}
 }
