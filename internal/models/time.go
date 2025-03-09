fed571c49947f57d10c2e3b683e159e06f3828fa
daioru <daioru.xyz@gmail.com>
2025-03-09 21:01:41 +0300

    Update JSONTime formatting

--- internal/models/time.go
+++ internal/models/time.go
@@ -7,8 +7,8 @@ import (
 
 type JSONTime time.Time
 
-// Метод для маршалинга JSON
+
 func (t JSONTime) MarshalJSON() ([]byte, error) {
-	formatted := fmt.Sprintf(`"%s"`, time.Time(t).Format("2006-01-02 15:04:05"))
+	formatted := fmt.Sprintf(`"%s"`, time.Time(t).UTC().Format(time.RFC3339))
 	return []byte(formatted), nil
 }
